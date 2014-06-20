60013bf6313e102a308b07bca7528a0f66f4b8c8
Nidhin Pattaniyil <npatta01@gmail.com>
2014-06-19 22:38:55 -0400

    gitignore:  ignore RStudio file sample.R  added cachemean cachematrix:  implemented caching

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #set inverse of matrix to null
+  inv <-NULL
+  
+  #set new value of matrix and set inverse to null
+  set<-function(y){
+    x<<-y
+    inv <<-NULL
+  }
+  
+  #return matrix value
+  get <- function() x
+  
+  #set the new inverse
+  setinverse <- function(new_inv) inv <<- new_inv
+  
+  #get the inverser of hte matrix
+  getinverse <- function() inv
+  
+  #list of 4 functions set/get matrix value getmatrix/setmatrix
+  list(set=set, get=get, setinverse =setinverse, getinverse=getinverse)
+  
+  
 }
 
 
@@ -12,4 +33,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  #get inverse
+  inv <- x$getinverse()
+  #if inverse is not null, return it
+  if (!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  #get matrix value
+  data <- x$get()
+  #compute inverse
+  inv <- solve(data,...)
+  #save inverse
+  x$setinverse(inv)
+  #return inverse
+  inv
+  
 }
